a6865777e7e39aabbc8d1ccc6d1e70e7b43f30df
RakeshRevashetti <rakeshyatish@gmail.com>
2025-05-01 16:42:30 +0530

    key name

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "terra"                  # Names of key in aws
+  default = "rakeshrr_pt"                  # Names of key in aws
 }
 
 
